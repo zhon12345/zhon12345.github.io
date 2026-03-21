98a34ecbe33d30d2bd93c7712f872c9f47c463ad
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2026-03-21 22:54:05 +0800

    🚧 Revamp typography and color system

--- src/components/Header/MobileMenu.vue
+++ src/components/Header/MobileMenu.vue
@@ -6,7 +6,9 @@
     @pointerup="isMenuOpen = !isMenuOpen"
     aria-label="Toggle Dropdown Menu"
   >
-    <div class="*:bg-text cursor-pointer space-y-1.5 *:block *:h-0.5 *:w-6 *:duration-200 *:motion-reduce:duration-50">
+    <div
+      class="*:bg-text cursor-pointer space-y-1.5 *:block *:h-0.5 *:w-6 *:duration-200 *:motion-reduce:duration-50"
+    >
       <span :class="{ 'translate-y-2 rotate-45': isMenuOpen }"></span>
       <span :class="{ 'scale-0': isMenuOpen }"></span>
       <span :class="{ '-translate-y-2 -rotate-45': isMenuOpen }"></span>
@@ -17,10 +19,19 @@
   <Teleport to="#dropdown">
     <ul
       ref="dropdown"
-      class="bg-surface fixed top-16 left-0 flex w-full flex-col gap-3 pb-5 text-lg shadow-xl/20 duration-400 motion-reduce:duration-100 md:hidden"
-      :class="isMenuOpen ? 'pointer-events-auto translate-y-0' : 'pointer-events-none -translate-y-full'"
+      class="bg-surface-elevated dark:bg-surface-muted fixed top-16 left-0 flex w-full flex-col gap-3 pb-5 text-lg duration-400 motion-reduce:duration-100 md:hidden"
+      :class="
+        isMenuOpen
+          ? 'pointer-events-auto translate-y-0'
+          : 'pointer-events-none -translate-y-full'
+      "
     >
-      <li v-for="item in props.menuItems" :key="item.name" class="w-full text-center" @click="isMenuOpen = false">
+      <li
+        v-for="item in props.menuItems"
+        :key="item.name"
+        class="w-full text-center"
+        @click="isMenuOpen = false"
+      >
         <a :href="item.href" class="hover:underline">
           {{ item.name }}
         </a>
@@ -42,7 +53,12 @@ const dropdown = ref<HTMLElement | null>(null);
 
 function handleClickOutside(event: MouseEvent) {
   const target = event.target as Node;
-  if (dropdown.value && !dropdown.value.contains(target) && hamburger.value && !hamburger.value.contains(target)) {
+  if (
+    dropdown.value &&
+    !dropdown.value.contains(target) &&
+    hamburger.value &&
+    !hamburger.value.contains(target)
+  ) {
     isMenuOpen.value = false;
   }
 }
