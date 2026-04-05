f3b1fdd4856f4788dc12e8f2ae18bc6c15f0158f
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2026-04-05 22:59:09 +0800

    ♿️ Add external link icon to external links

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,24 +1,42 @@
+import type { Root, Element } from "hast";
 import { unified } from "unified";
-import { defaultSchema } from "rehype-sanitize";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
+import rehypeParse from "rehype-parse";
 import rehypeExternalLinks from "rehype-external-links";
-import rehypeSanitize from "rehype-sanitize";
+import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
+import BoxArrowUpRight from "@/components/Icons/BoxArrowUpRight.svg?raw";
 
 const schema = {
 	...defaultSchema,
+	tagNames: [...(defaultSchema.tagNames || []), "svg", "path"],
 	attributes: {
 		...defaultSchema.attributes,
 		a: [...(defaultSchema.attributes?.a || []), "target", "rel"],
+		span: [...(defaultSchema.attributes?.span || []), "className"],
+		svg: [...(defaultSchema.attributes?.svg || []), "viewBox", "fill", "className"],
+		path: [...(defaultSchema.attributes?.path || []), "d"],
 	},
 };
+
+const svgNode: Element = (() => {
+	const parsed = unified().use(rehypeParse, { fragment: true }).parse(BoxArrowUpRight) as Root;
+	const svg = parsed.children?.[0] as Element;
+	svg.properties = { ...(svg.properties || {}), fill: "currentColor", className: ["inline-block", "w-4"] };
+	return svg;
+})();
 
 export function markdownToHtml(markdown: string) {
 	const file = unified()
 		.use(remarkParse)
 		.use(remarkRehype)
-		.use(rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] })
+		.use(rehypeExternalLinks, {
+			target: "_blank",
+			rel: ["noopener", "noreferrer"],
+			content: [svgNode],
+			contentProperties: { className: ["ml-1"] },
+		})
 		.use(rehypeSanitize, schema)
 		.use(rehypeStringify)
 		.processSync(markdown);
